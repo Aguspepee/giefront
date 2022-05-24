d0c6f291b05b67a4fd9ca4f05441f6d7f3427b0d
Aguspepee <sanchezagustinezequiel@gmail.com>
2022-05-24 16:13:06 -0300

    Cambios en Axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 export default axios.create({
  //baseURL: "http://localhost:9000/"
- //baseURL: "http://195.179.193.227/"
+ //baseURL: "http://195.179.193.227/api/"
  baseURL: process.env.REACT_APP_BACKEND_URL
 //  baseURL: "https://backmant.herokuapp.com/"
 })
